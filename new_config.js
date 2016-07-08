fc40c2eeebca60f15c727f47ee633aa7990969cb
Maurício R Duarte <mauriciotem@gmail.com>
2016-07-08 10:31:32 -0300

    Add Proponente ajustado

--- new_config.js
+++ new_config.js
@@ -29,21 +29,38 @@ function ajustaProponentes() {
 
 	if( valCampoProponente == "" ) {
 		setValor(selCampoProponente, 0);
+		addProponente();
 	}
+
+	if(parseInt(getValor("#proponente")) > 0) {
+		var proponente = parseInt(getValor("#proponente"));
+		setValor("#proponente", proponente);
+		Array.prototype.forEach.call(document.querySelectorAll('.tc-div-proponente'), function(el, i) {
+			el.style.display = "none";
+		});		
+		Array.prototype.some.call(document.querySelectorAll('.tc-div-proponente'), function(el, i) {
+			el.style.display = '';
+			return i+1 == proponente;
+		});
+	}
+
 }
 
 function addProponente() {
 	var maxProp = 4;
 	var campoProponente = "#proponente";
-	var proponente = parseInt(getValor(campoProponente) + 1);
+	var proponente = parseInt(getValor(campoProponente)) + 1;
 
 	if( parseInt(getValor(campoProponente)) < maxProp ) {
 		setValor(campoProponente, proponente);
-		document.querySelector('.tc-div-proponente').style.display = "none";
 		Array.prototype.forEach.call(document.querySelectorAll('.tc-div-proponente'), function(el, i) {
+			el.style.display = "none";
+		});		
+		Array.prototype.some.call(document.querySelectorAll('.tc-div-proponente'), function(el, i) {
+			console.log("div " + i);
+			console.log(proponente);
 			el.style.display = '';
-			if((index+1) == proponente)
-				return false;
+			return i+1 === proponente;			
 		});
 	}
 
@@ -62,6 +79,8 @@ function addProponente() {
 
 
 }
+
+ajustaProponentes();
 
 // EVENTOS
 document.querySelector(".tc-btnAddProp").addEventListener("click", addProponente);
