9b354001c4dee2080a9e3d2df28f940a1ed772ae
Maurício R Duarte <mauriciord@me.com>
2016-07-07 22:10:50 -0300

    Funcao addProponente

--- new_config.js
+++ new_config.js
@@ -21,7 +21,7 @@ function setValor(el, valor) {
 	}
 
 }
-
+// PROPONENTES
 function ajustaProponentes() {
 	var selCampoProponente = "#proponente";
 	var campoProponente = document.querySelector(selCampoProponente);
@@ -31,3 +31,37 @@ function ajustaProponentes() {
 		setValor(selCampoProponente, 0);
 	}
 }
+
+function addProponente() {
+	var maxProp = 4;
+	var campoProponente = "#proponente";
+	var proponente = parseInt(getValor(campoProponente) + 1);
+
+	if( parseInt(getValor(campoProponente)) < maxProp ) {
+		setValor(campoProponente, proponente);
+		document.querySelector('.tc-div-proponente').style.display = "none";
+		Array.prototype.forEach.call(document.querySelectorAll('.tc-div-proponente'), function(el, i) {
+			el.style.display = '';
+			if((index+1) == proponente)
+				return false;
+		});
+	}
+
+	if(parseInt(getValor(campoProponente)) == 1) {
+		document.querySelector('#btn_adicionaProp').disabled = false;
+		document.querySelector('#btn_deletaProp').disabled = true;
+	}
+	if(parseInt(getValor(campoProponente)) >= maxProp) {
+		document.querySelector('#btn_adicionaProp').disabled = true;
+		document.querySelector('#btn_deletaProp').disabled = false;
+	}
+	if(parseInt(getValor(campoProponente)) > 1 && parseInt(getValor(campoProponente)) < maxProp) {
+		document.querySelector('#btn_adicionaProp').disabled = false;
+		document.querySelector('#btn_deletaProp').disabled = true;
+	}
+
+
+}
+
+// EVENTOS
+document.querySelector(".tc-btnAddProp").addEventListener("click", addProponente);
